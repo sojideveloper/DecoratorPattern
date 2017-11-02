781cc055e77bc37f999dfa6fcd1ac347857bbcf3
sojideveloper <sojideveloper@users.noreply.github.com>
2017-11-02 16:35:10 -0500

    First commit

--- DecoratorPattern/ViewController.swift
+++ DecoratorPattern/ViewController.swift
@@ -8,18 +8,123 @@
 
 import UIKit
 
+// DECORATOR PATTERN: -
+// Allows you to add behaviors to objects while not requiring
+// all objects to adopt the behaviors.
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        // MARK: Decorator implementation
+        
+        let dialer = DefaultDialer()
+        
+        // If you'd like to call people on their other number
+        // after not reaching their first number
+        let followUpCallDialerDecorator = FollowupCallDialerDecorator(dialer: dialer)
+        
+        // If you'd like to call peoples' cell phones after not reaching them on landlines
+        let cellPhoneDialer = CellPhoneDialerDecorator(dialer: followUpCallDialerDecorator)
+        
+        // To text cell phones if not able to reach them via calls
+        let textMe = TextMeDecorator(dialer: cellPhoneDialer)
+        textMe.contactAction() // Contact action to implement all contact methods
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
 
 }
+
+
+// MARK: DialerInterface protocol
+
+protocol DialerInterface {
+    func contactAction()
+}
+
+
+// MARK: DefaultDialer
+
+struct DefaultDialer: DialerInterface {
+    
+    func contactAction() {
+        
+        print("--------Begin---------")
+        print("Default Dialing Action completed")
+        
+    }
+    
+}
+
+
+// MARK: - FollowupCalDialerlDecorator
+
+struct FollowupCallDialerDecorator: DialerInterface {
+    
+    // Since the decorator adheres to the DialerInterface protocol
+    // we are able to use the old functionality as-is while
+    // adding a new behavior.
+
+    let dialer: DialerInterface
+    
+    init(dialer: DialerInterface) {
+        self.dialer = dialer // Takes the original dialer object in the constructor.
+    }
+    
+    func contactAction() {
+        dialer.contactAction()
+        
+        print("----------Followup Call-------------")
+        print("Follow-up Dialin Action completed")
+
+    }
+}
+
+
+// MARK: - CellPhoneDialerDecorator
+
+struct CellPhoneDialerDecorator: DialerInterface {
+    
+    let dialer: DialerInterface
+    
+    init(dialer: DialerInterface) {
+        self.dialer = dialer
+    }
+    
+    func contactAction() {
+        dialer.contactAction()
+        
+        print("--------Text----------")
+        print("Cell Phone Action completed")
+
+    }
+    
+}
+
+
+// MARK: - TextMeDecorator
+
+struct TextMeDecorator: DialerInterface {
+    let dialer: DialerInterface
+    
+    init(dialer: DialerInterface) {
+        self.dialer = dialer
+    }
+    
+    func contactAction() {
+        dialer.contactAction()
+        
+        print("-------Text---------")
+        print("Text Me Action completed")
+
+    }
+    
+}
+
+
+
+
+
+
 
